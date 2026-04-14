0f816bc6936b87a8a4abe519e97f57a5a4a1f4be
Qingsong Chen <changxian.cqs@antgroup.com>
2026-04-14 11:37:58 +0000

    Enable LTP rename tests with a scratch disk

--- test/initramfs/src/conformance/ltp/run_ltp_test.sh
+++ test/initramfs/src/conformance/ltp/run_ltp_test.sh
@@ -7,6 +7,19 @@ TEST_TMP_DIR=${CONFORMANCE_TEST_WORKDIR:-/tmp}
 LOG_FILE=$TEST_TMP_DIR/result.log
 RESULT=0
 
+# Some test cases require a block device. Select the first unused `/dev/vd*`
+# device that is not mounted and export it as `LTP_DEV`.
+if [ -z "${LTP_DEV}" ]; then
+    for candidate in /dev/vd[a-z]; do
+        [ -b "$candidate" ] || continue
+        if ! grep -q "^${candidate}[0-9]* " /proc/mounts; then
+            LTP_DEV="$candidate"
+            break
+        fi
+    done
+fi
+export LTP_DEV
+
 rm -f $LOG_FILE
 CREATE_ENTRIES=1 $LTP_DIR/runltp -f syscalls -Q -p -d $TEST_TMP_DIR -l $LOG_FILE
 if [ $? -ne 0 ]; then
--- tools/qemu_args.sh
+++ tools/qemu_args.sh
@@ -99,6 +99,7 @@ COMMON_QEMU_ARGS="\
     -device isa-debug-exit,iobase=0xf4,iosize=0x04 \
     -drive if=none,format=raw,id=x0,file=./test/initramfs/build/ext2.img \
     -drive if=none,format=raw,id=x1,file=./test/initramfs/build/exfat.img \
+    -drive if=none,format=raw,id=x2,file=./test/initramfs/build/test.img \
 "
 
 if [ "$1" = "iommu" ]; then
@@ -122,6 +123,7 @@ if [ "$1" = "microvm" ]; then
         -no-user-config \
         -device virtio-blk-device,drive=x0,serial=vext2 \
         -device virtio-blk-device,drive=x1,serial=vexfat \
+        -device virtio-blk-device,drive=x2,serial=vtest \
         -device virtio-keyboard-device \
         -device virtio-net-device,netdev=net01 \
         -device virtio-serial-device \
@@ -133,6 +135,7 @@ else
         -machine q35,kernel-irqchip=split \
         -device virtio-blk-pci,bus=pcie.0,addr=0x6,drive=x0,serial=vext2,disable-legacy=on,disable-modern=off,queue-size=64,num-queues=1,request-merging=off,backend_defaults=off,discard=off,write-zeroes=off,event_idx=off,indirect_desc=off,queue_reset=off$IOMMU_DEV_EXTRA \
         -device virtio-blk-pci,bus=pcie.0,addr=0x7,drive=x1,serial=vexfat,disable-legacy=on,disable-modern=off,queue-size=64,num-queues=1,request-merging=off,backend_defaults=off,discard=off,write-zeroes=off,event_idx=off,indirect_desc=off,queue_reset=off$IOMMU_DEV_EXTRA \
+        -device virtio-blk-pci,bus=pcie.0,addr=0x8,drive=x2,serial=vtest,disable-legacy=on,disable-modern=off,queue-size=64,num-queues=1,request-merging=off,backend_defaults=off,discard=off,write-zeroes=off,event_idx=off,indirect_desc=off,queue_reset=off$IOMMU_DEV_EXTRA \
         -device virtio-net-pci,netdev=net01,disable-legacy=on,disable-modern=off$VIRTIO_NET_FEATURES$IOMMU_DEV_EXTRA \
         -device virtio-serial-pci,disable-legacy=on,disable-modern=off$IOMMU_DEV_EXTRA \
         $CONSOLE_ARGS \
