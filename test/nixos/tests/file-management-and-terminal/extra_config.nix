8c7fd088d858a26b5f490e0034cf6d1f6baf6b87
Qingsong Chen <changxian.cqs@antgroup.com>
2026-04-29 09:17:50 +0000

    Add test for file management and terminal applications

---
+++ test/nixos/tests/file-management-and-terminal/extra_config.nix
@@ -0,0 +1,31 @@
+{ config, lib, pkgs, ... }:
+
+{
+  environment.systemPackages = with pkgs; [
+    bzip2
+    gzip
+    p7zip
+    gnutar
+    xz
+    zip
+    unzip
+    screen
+    file
+    bat
+    gawk
+    sd
+    gnused
+    eza
+    fd
+    fzf
+    ripgrep
+    silver-searcher
+    tree
+    age
+    crunch
+    gnupg
+    john
+    restic
+    wipe
+  ];
+}
